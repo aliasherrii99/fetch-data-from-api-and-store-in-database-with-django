3c7122585ac8fc7a6bf308dfa9e8152350da0750
aliasherrii99 <aliasherinejad@gmail.com>
2025-01-06 12:33:48 +0330

    new change

--- task3/views.py
+++ task3/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from task3.visualization import list_of_fund_type, list_of_asset_under_maangement
+from task3.visualization import list_of_fund_type3, list_of_asset_under_maangement2
 
 
 # Create your views here.
 def fund_type_view(request):
-    fund_data = zip(list_of_fund_type, list_of_asset_under_maangement)
+    fund_data = zip(list_of_fund_type3, list_of_asset_under_maangement2)
     return render(request, 'fund.html', {'fund_data': fund_data})
--- task3/visualization.py
+++ task3/visualization.py
@@ -20,3 +20,36 @@ def fund_type_total():
 
 
 list_of_fund_type, list_of_asset_under_maangement = fund_type_total()[0], fund_type_total()[1]
+list_of_fund_type2 = sorted(list_of_fund_type, reverse=True)
+list_of_asset_under_maangement2 = []
+for i in list_of_fund_type2:
+    list_of_asset_under_maangement2.append(list_of_asset_under_maangement[list_of_fund_type.index(i)])
+
+name_from_fund_type = {
+    4: 'در اوراق بهادار با درآمد ثابت',
+    5: 'در اوراق بهادار مبتنی بر سپرده کالایی',
+    6: 'در سهام',
+    7: 'مختلط',
+    11: 'اختصاصی بازارگردانی',
+    12: 'جسورانه',
+    13: 'پروژه‌ای',
+    14: 'زمین و ساختمان',
+    16: 'خصوصی',
+    17: 'صندوق در صندوق',
+    18: 'املاک و مستغلات',
+    21: 'در سهام-بخشی',
+    22: 'در سهام-سهامی اهرمی',
+    23: 'در سهام-شاخصی',
+    24: 'با تضمین اصل مبلغ سرمایه گذاری',
+    25: 'بازنشستگی تکمیلی'
+}
+list_of_fund_type3 = []
+for i in list_of_fund_type2:
+    list_of_fund_type3.append(name_from_fund_type[i])
+
+# def show():
+#     # print(list(zip(list_of_fund_type, list_of_asset_under_maangement)))
+#     print(list(zip(list_of_fund_type2, list_of_asset_under_maangement2)))
+
+# py manage.py shell
+# from task3.visualization import show
